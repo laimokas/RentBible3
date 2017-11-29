cb10d6f4b31eb7a44f4a5a8a5a48a59d45d99630
c16m1rzx <c16m1rzx@C16M1RZX-PC>
2017-11-29 22:55:57 +0200

    Add project files.

---
+++ RentBible3/RentBible3.HTMLClient/Screens/TenanciesSet/AddEditTenancies1.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditTenancies1.TenancyOverallValuation_postRender = function (element, contentItem) {
+    // Write code here.
+
+};
---
+++ RentBible3/RentBible3.HTMLClient/Screens/TenanciesSet/ViewTenancies1.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewTenancies1.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Tenancy.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Tenancy." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
