e5cd7818cda18f58f7d065f1f0284d960acd622f
laimokas <laimokas@gmail.com>
2017-11-30 00:14:18 +0200

    Hashing

--- RentBible3/RentBible3.Server/DataSources/ApplicationData/_ApplicationDataService.lsml.cs
+++ RentBible3/RentBible3.Server/DataSources/ApplicationData/_ApplicationDataService.lsml.cs
@@ -4,14 +4,23 @@ using System.Text;
 using System.Linq;
 using System.Collections.Generic;
 using System;
+using HashLibrary;
 
 namespace LightSwitchApplication
 {
     public partial class ApplicationDataService
     {
-        partial void Tenancies_Inserted(Tenancy entity)
+        partial void Tenancies_Inserting(Tenancy entity)
         {
-             
+            var hasher = new Hasher(20, 20);
+
+            var hashedTenant = hasher.HashPassword(string.Format("{0}_{1}", entity.TenantName, entity.TenantCode));
+            var hashedLessor = hasher.HashPassword(string.Format(entity.LessorName + entity.LessorCode));
+
+            entity.TenantId = hashedTenant.Hash;
+            entity.TenantSalt = hashedTenant.Salt;
+            entity.LessorId = hashedLessor.Hash;
+            entity.LessorSalt = hashedLessor.Salt;
         }
     }
 }
